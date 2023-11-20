581cbde9e6ca4803b5143af0b219582b6c251b18
RafSchapendonk <rafschapendonk@outlook.com>
2023-11-20 13:39:29 +0100

    finishing basic api setup before implementing values

--- src/controllers/recommendationController.ts
+++ src/controllers/recommendationController.ts
@@ -16,13 +16,17 @@ export default class RecommendationController extends AppController {
 
   public async recommendEvent(req: Request, res: Response): Promise<void> {
     // Test data
-    var playlist = [7, 8, 9, 1000, 20000];
+    // Data order should be [Danceability, Energy, Loudness, Speechiness, Acousticness, Instrumentalness, Liveness, Valence, Tempo]
+    // All data should be scaled down to a value between 0 and 1
+    var playlist = [
+      0.633, 0.162, 0.104, 0.475, 0.923, 0.933, 0.252, 0.195, 0.726,
+    ];
     var events: { [key: string]: number[] } = {
-      event1: [5, 4, 3, 2, 1],
-      event2: [1, 2, 3, 4, 5],
-      event3: [3, 4, 5, 4, 3],
-      event4: [1, 2, 3, 4, 5],
-      event5: [1, 2, 3, 4, 5],
+      event1: [0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791],
+      event2: [0.246, 0.579, 0.802, 0.135, 0.468, 0.791, 0.024, 0.357, 0.68],
+      event3: [0.369, 0.702, 0.925, 0.258, 0.591, 0.814, 0.147, 0.48, 0.703],
+      event4: [0.492, 0.825, 0.158, 0.381, 0.714, 0.937, 0.27, 0.603, 0.826],
+      event5: [0.615, 0.948, 0.271, 0.604, 0.937, 0.16, 0.493, 0.826, 0.159],
     };
 
     const recommendationsLogic = new RecommendationsLogic(playlist, events);
